c378c500f732c5e4956a1e2b81cd7392d0ea06c2
pankaj-gitcode <pankajaditya75@gmail.com>
2023-08-04 21:59:49 +0530

    updated modues1&2.mjs files

---
+++ module1.mjs
@@ -0,0 +1,9 @@
+import ui, {d,c,b,a} from './module2.mjs'
+
+//invoke the ui
+console.log(ui)
+
+//console the named exported modules
+// console.log(d)
+// console.log(b)
+console.log(c,a,d)
---
+++ module2.mjs
@@ -0,0 +1,13 @@
+//create some variables
+const a = "Raj";
+const b = "Rohan";
+const c = "John";
+const d = "Alexa";
+
+//export deafult module contents
+// export default a;
+export default {a, b,c,d};  //we can export like this too
+
+//export using name
+export {d,a,b,c};
+
