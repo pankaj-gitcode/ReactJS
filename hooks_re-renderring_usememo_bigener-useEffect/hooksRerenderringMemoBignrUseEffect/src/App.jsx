64080ba933290e63f24d9fe3f4890c93d685c77b
pankaj-gitcode <pankajaditya75@gmail.com>
2024-01-08 21:54:34 +0530

    stop-rerender_using_memo()_for_non-changing-props_component

--- hooks_re-renderring_usememo_bigener-useEffect/hooksRerenderringMemoBignrUseEffect/src/App.jsx
+++ hooks_re-renderring_usememo_bigener-useEffect/hooksRerenderringMemoBignrUseEffect/src/App.jsx
@@ -1,31 +1,33 @@
-import React, {useState} from 'react'
+import React, {useState, memo} from 'react'
+
 export default function App(){
-  return(
-    <div>
-      <Header title="Header1"/>
-      <HeaderWithButton/>
-      <Header title="Header2"/>
-      <Header title="Header3"/>
-      <Header title="Header4"/>
-    </div>
-  )
-}
-//separate button with Header to change content
-function HeaderWithButton(){
-  const [content, setContent] = useState('Raj');
-  function contentChange(){
-    setContent(Math.floor(Math.random()*100));
+  const [title, setTitle] = useState('Raj');
+
+  function clickButton(){
+   setTitle("The new content is: " + Math.random());
   }
+
   return(
     <div>
-      <button onClick={contentChange}>Click here to change the content</button>
-      <Header title={content}/>
+      <button onClick={clickButton}>Click me</button>
+      <Header title={title}/>
+      <Header title='John-1'/>
+      <Header title='John-2'/>
+      <Header title='John-3'/>
     </div>
   )
 }
-//Header function
-function Header({title}){
+
+//component-Header:=> It's prop is not changin,but when we click on button in App() it also renders
+// function Header({title}){
+//   return(
+//     <div>{title}</div>
+//   )
+// }
+
+//to stop we need to use "memo" for the component whose prop doenot changes but it's being re-rendering
+const Header = memo(({title})=>{
   return(
     <div>{title}</div>
   )
-}
+})
