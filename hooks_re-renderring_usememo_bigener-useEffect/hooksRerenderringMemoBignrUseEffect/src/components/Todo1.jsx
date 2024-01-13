b288528705ca484ea5476891ef169c61b624ba91
pankaj-gitcode <pankajaditya75@gmail.com>
2024-01-13 18:10:22 +0530

    todo-v1

---
+++ hooks_re-renderring_usememo_bigener-useEffect/hooksRerenderringMemoBignrUseEffect/src/components/Todo1.jsx
@@ -0,0 +1,45 @@
+import React, {useState} from 'react'
+
+export default function Todo1(){
+    //state array with 3 todos
+    const [todos, setTodos] = useState([{
+        id:1,
+        title: 'sports',
+        description: 'Learn Martial Arts'
+    },{
+        id:2,
+        title: 'certifications',
+        description: 'Javascript certifications'
+    },{
+        id: 3,
+        title: 'food',
+        description: 'Paneer kurma'
+    }])
+
+    function addTodo(){
+        setTodos([...todos, {
+            id:4, 
+            title: Math.floor(Math.random() * 10), 
+            description: Math.floor(Math.random() * 10)}])
+    }
+    
+    return(
+        <div>
+            {/* button in top level App coomponent to add new TODO */}
+            <button onClick={addTodo}>Add Todos</button>
+            {/* Iterate over the array and render all todos */}
+            {todos.map((todo)=> <Todo key={todo.id} title={todo.title} description={todo.description}  />)}
+        </div>
+    )
+}
+
+
+//Todo component that accepts title, description as input
+function Todo({title, description}){
+    return(
+        <div>
+            <h4>{title}</h4>
+            <h6>{description}</h6>
+        </div>
+    )
+}
--- hooks_re-renderring_usememo_bigener-useEffect/hooksRerenderringMemoBignrUseEffect/src/main.jsx
+++ hooks_re-renderring_usememo_bigener-useEffect/hooksRerenderringMemoBignrUseEffect/src/main.jsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import MemoComp from './components/MemoComp.jsx'
+import Todo1 from './components/Todo1.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* <App /> */}
-    <MemoComp />
+    {/* <MemoComp /> */}
+    <Todo1 />
   </React.StrictMode>,
 )
