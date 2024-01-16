b7d1f3ddc18d2ed279c45af8f5202634ac3fa6b7
pankaj-gitcode <pankajaditya75@gmail.com>
2024-01-17 04:16:49 +0530

    useEffect-6.2-v3

--- useEffect/useeffect6.2/src/App.jsx
+++ useEffect/useeffect6.2/src/App.jsx
@@ -1,17 +1,35 @@
-import React, {useEffect, useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import './App.css'
 
 export default function App(){
+  const [cnt, setCnt] = useState(1)
+  
+  function ch1(){
+    setCnt(1)
+  }
+  function ch2(){
+    setCnt(2)
+  }
+  function ch3(){
+    setCnt(3)
+  }
+  function ch4(){
+    setCnt(4)
+  }
 
   return(
     <div>
-      <Todo id={2}/>
+      <button onClick={ch1}>1</button>
+      <button onClick={ch2}>2</button>
+      <button onClick={ch3}>3</button>
+      <button onClick={ch4}>4</button>
+
+      <Todo id={cnt}/>
     </div>
   )
 }
 
-//Todo component
-function Todo({id}){
+function Todo ({id}){
   const [todo, setTodo] = useState([]);
 
   useEffect(()=>{
@@ -20,12 +38,14 @@ function Todo({id}){
       const response = await res.json();
       setTodo(response.todo)
     })
-  }, [])
+  }, [id])
 
   return(
     <div>
+      id:{id}
       <h1>{todo.title}</h1>
       <h5>{todo.description}</h5>
     </div>
   )
+
 }
