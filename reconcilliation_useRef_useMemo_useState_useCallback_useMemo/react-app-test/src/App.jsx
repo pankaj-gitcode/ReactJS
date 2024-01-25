0c4c1fc4a5946b62374de40b445103cfd27c1bb6
pankaj-gitcode <pankajaditya75@gmail.com>
2024-01-26 04:08:05 +0530

    disadvantage_of_using_useEffect_over_expensive_computation

--- reconcilliation_useRef_useMemo_useState_useCallback_useMemo/react-app-test/src/App.jsx
+++ reconcilliation_useRef_useMemo_useState_useCallback_useMemo/react-app-test/src/App.jsx
@@ -1,38 +1,58 @@
-import React, {useState, useEffect} from 'react'
+//useEffect cons: let's say 2 exchange investment i've exchange1Data & exchange2Data and one bankData
+
+import React, {useState, useEffect, useMemo} from 'react'
 import './App.css'
 
 export default function App(){
-  const [todoList, setTodoList] = useState([]);
+  // 2 exchange data & 1 bank data
+  const [exchange1Data, setExchange1Data] = useState({});
+  const [exchnage2Data, setExchange2Data] = useState({});
+  const [bankData, setBankData] = useState({});
+
+  //useEffect for all individuals
+  useEffect(()=>{
+    setExchange1Data({return: 100});
+    console.log('ue-1')
+  },[])
 
   useEffect(()=>{
-    fetch("https://sum-server.100xdevs.com/todos")
-    .then(async (res)=>{
-      const response = await res.json();
-      setTodoList(response.todos);
-      console.log("Render-1")
-    })
-    .catch((err)=>console.error(`ERROR: ${err}`))
+    setExchange2Data({return: 100});
+    console.log('ue-2')
   }, [])
 
-   useEffect(()=>{
-     setTimeout(()=>{
-       console.log("Render-2")
-     });
-   }, [])
+  useEffect(()=>{
+    setTimeout(()=>{
+      setBankData({return:100})
+      console.log('timeout')
+    }, 5000)
+  }, [])
+  console.log("Render-1");
+  const totalExchangeData = exchange1Data.return + exchnage2Data.return;
+  const tax = (totalExchangeData + bankData.return) * 0.3
+  console.log("Render-2");
 
   return(
     <div>
-
-     {
-      todoList.length>0? (
-        todoList.map((todo)=>(
-          <h5 key={todo.id}> {todo.title} {todo.description}</h5>
-        ))
-      ):(
-        <p>Loading...</p>
-      )
-     }       
-      
+      <h4>Total tax is: {[tax , totalExchangeData]}</h4>
+      <h6> {console.log("hi") }</h6>
     </div>
   )
 }
+
+
+
+//  OUTPUT:👇
+//       Render-1
+//       Render-2
+//       hi
+
+//       ue-1
+//       ue-2
+//       Render-1
+//       Render-2
+//       hi
+
+//       timeout
+//       Render-1
+//       Render-2
+//       hi
