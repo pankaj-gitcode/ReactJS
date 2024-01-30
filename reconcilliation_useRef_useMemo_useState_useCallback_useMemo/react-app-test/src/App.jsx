bbd21f89b347f681abab4f1a00a0719dd0d70e32
pankaj-gitcode <pankajaditya75@gmail.com>
2024-01-30 12:34:46 +0530

    DOM_override

--- reconcilliation_useRef_useMemo_useState_useCallback_useMemo/react-app-test/src/App.jsx
+++ reconcilliation_useRef_useMemo_useState_useCallback_useMemo/react-app-test/src/App.jsx
@@ -1,39 +1,16 @@
-import React, {useState, memo, useCallback} from 'react'
+import React, {useEffect, useRef, useState} from 'react'
 
 export default function App(){
-  const [counter, setCounter] = useState(0);
+  const [name, setName] = useState('Raj');
 
-  const click = ()=>{
-    setCounter(counter+1);
-  }
-  const value = 100;
-  const value1 = 200;
+  useEffect(()=>{
+    setTimeout(()=>{document.getElementById('ref').innerHTML = "Hello! this is John..."}, 3000);
+    console.log(document.getElementById('ref'))
+  }, []);
 
-  const func1 = useCallback(()=>{
-    const a = 100;
-    return a;
-  }, [])
   return(
     <div>
-      <button onClick={click}>Counter: {counter}</button>
-      <Display title={value}/>
-      <Display title={value1}/>
-      <Display1 val={func1} />
-      
+      <h1 id="ref">Hi there, this is {name}</h1>
     </div>
   )
 }
-
-const Display =memo(({title})=>{
-  console.log("This is re-rendered!!")
-  return(
-    <div>display: {title}</div>
-  )
-})
-
-const Display1 = memo(({val})=>{
-  console.log("function re-renders")
-  return(
-    <div>Value: {val()}</div>
-  )
-})
