f2d73a7ffcfd4501c61496f873092b5cb43f0990
pankaj-gitcode <pankajaditya75@gmail.com>
2024-01-30 12:19:02 +0530

    memo_useCallback

--- reconcilliation_useRef_useMemo_useState_useCallback_useMemo/react-app-test/src/App.jsx
+++ reconcilliation_useRef_useMemo_useState_useCallback_useMemo/react-app-test/src/App.jsx
@@ -1,42 +1,39 @@
-import React, {useState, useEffect, useMemo} from 'react'
-import './App.css'
+import React, {useState, memo, useCallback} from 'react'
 
 export default function App(){
-  const [exchange1Data, setExchange1Data] = useState({});
-  const [exchange2Data, setExchange2Data] = useState({});
-  const [bankData, setBankData] = useState({});
+  const [counter, setCounter] = useState(0);
 
-  useEffect(()=>{
-    setExchange1Data({return:100});
-    console.log('UE-1')
+  const click = ()=>{
+    setCounter(counter+1);
+  }
+  const value = 100;
+  const value1 = 200;
+
+  const func1 = useCallback(()=>{
+    const a = 100;
+    return a;
   }, [])
-
-  useEffect(()=>{
-    setExchange2Data({return: 100});
-    console.log("UE-2")
-  }, [])
-
-  useEffect(()=>{
-    setTimeout(()=>{
-      setBankData({return:100});
-      console.log("UE-3 | Timeout")
-    }, 5000);
-  }, [])
-
-  //useMemo
-  const exchangeData = useMemo(()=>{
-    console.log("useMemo...")
-    const totalExchangeData = exchange1Data.return + exchange2Data.return;
-    return totalExchangeData;
-  }, [exchange1Data, exchange2Data]);
-
-  const tax = exchangeData + bankData.return * 0.3 ; 
-  console.log("Expensive Calculation");
-
   return(
     <div>
-      <h1>Total Tax: {tax}</h1>
-      {console.log("CS")}
+      <button onClick={click}>Counter: {counter}</button>
+      <Display title={value}/>
+      <Display title={value1}/>
+      <Display1 val={func1} />
+      
     </div>
   )
 }
+
+const Display =memo(({title})=>{
+  console.log("This is re-rendered!!")
+  return(
+    <div>display: {title}</div>
+  )
+})
+
+const Display1 = memo(({val})=>{
+  console.log("function re-renders")
+  return(
+    <div>Value: {val()}</div>
+  )
+})
