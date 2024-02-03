563e5de1040a5e8e71e9f61f9e02ee67339bdebd
pankaj-gitcode <pankajaditya75@gmail.com>
2024-02-04 04:56:16 +0530

    react-router-dom_useNavigate_stop_refreshing_v2

--- reactRouter/src/App.jsx
+++ reactRouter/src/App.jsx
@@ -1,23 +1,19 @@
-import React, {useState,} from 'react'
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import React from 'react'
+import {BrowserRouter, Routes, Route, useNavigate} from 'react-router-dom'
 import './App.css'
 import { Landing } from './components/Landing'
 import { Dashboard } from './components/Dashboard'
 
 
 export default function App(){
-
-  const landingFunc = ()=>{ window.location.href='/' } //one way but not the perfect way
-  const dashboardFunc = ()=>{ window.location.href='/dashboard' } //window.location... uses hard reloading
-
+  
   return(
     <div>
+    <BrowserRouter>
       <div style={{backgroundColor:"#424242", color:"#fff"}}>
-        <button style={{marginRight:"100px"}} onClick={landingFunc}>Landing</button>
-        <button onClick={dashboardFunc}>Dashboard</button>
+        <AppBar />  
       </div>
 
-    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Landing/>} />
         <Route path="/dashboard" element={<Dashboard />} />
@@ -25,4 +21,17 @@ export default function App(){
     </BrowserRouter>
     </div>
   )
+}
+
+//Appbar component
+const AppBar = ()=>{
+  //initialise the navigate hook
+  const navigate = useNavigate();
+  return(
+    <div>
+      <button style={{margin:'100px'}} onClick={()=>{navigate('/')}}>Landing</button>
+      <button onClick={()=>{navigate('/dashboard')}}>Dashboard</button>
+    </div>
+  )
+
 }
