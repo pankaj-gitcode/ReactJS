34819e63d0926a72c2e444e606480264229e93d8
pankaj-gitcode <pankajaditya75@gmail.com>
2024-02-23 17:48:29 +0530

    rgh_simple_app_v1

--- recoil_deep_dive/src/App.jsx
+++ recoil_deep_dive/src/App.jsx
@@ -1,67 +1,49 @@
-//Home My Network (99+)Jobs (100)Messaging (112)Notifications (104)MeTotal (417)
+import React, { useMemo } from 'react'
 
-import React, { useEffect } from 'react'
-import { RecoilRoot, useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil'
-import { dataAtom, jobsAtom, messagingAtom, networkAtom, notificationsAtom, totalServiceSelector } from './atoms'
-import axios from 'axios'
+import './App.css'
+import { RecoilRoot, useRecoilValue, useSetRecoilState } from 'recoil'
+import { countAtom } from './atoms'
 
 export default function App(){
-  console.log("APP")
   return(
     <div>
-      <RecoilRoot>
-        <MainApp />
-      </RecoilRoot>     
-   </div>
+    <RecoilRoot>
+      <Count/>
+    </RecoilRoot>
+    </div>
   )
 }
 
-const MainApp = ()=>{
-  const networkCount = useRecoilValue(networkAtom); 
-  const jobsCount = useRecoilValue(jobsAtom);
-  const msgCount = useRecoilValue(messagingAtom);
-  const setMsgCount = useSetRecoilState(messagingAtom);
-  const notificationsCount = useRecoilValue(notificationsAtom);
-
-  const totalServicesCount = useRecoilValue(totalServiceSelector); 
-
-  const [dataCount, setDataCount] = useRecoilState(dataAtom);
-
-  useEffect(()=>{
-    const fetchData = async()=>{
-      try{
-        const response = await fetch("https://sum-server.100xdevs.com/notifications");
-        const res = await response.json();
-        setDataCount(res);
-      }catch(err){console.log(err)}
-    }
-    fetchData();
-  }, [totalServicesCount])
-
-//  useEffect(()=>{
-//   axios.get("https://sum-server.100xdevs.com/notifications")
-//   .then((res)=>{
-//     setDataCount(res.data);
-//     console.log(`Axios.get: ${res.data}`)
-//   })
-//  }, [msgCount, totalServicesCount])
-  
-  console.log(`DETAILS: ${dataCount.jobs}`)
-
-  // const serviceCount = useEffect
-
-  return (
+const Count = ()=>{
+  return(
     <div>
-      <button>Home</button>
-      <button>My Network ({dataCount.network})</button>
-      <button>Jobs ({jobsCount})</button>
-      <button>Messaging ({msgCount})</button>
-      <button>Notifications ({notificationsCount})</button>
-      <button onClick={()=>{setMsgCount(msgCount=>msgCount+1)}}>Me ({totalServicesCount})</button>
-      
-    </div>
-  );
+      <CountRender />
+      <Buttons />
+    </div> 
+  )
 }
 
+const CountRender = ()=>{
+  const count = useRecoilValue(countAtom);
+  const EvenOdd = useMemo(()=>{
+    return count%2==0?'Even':'Odd'
+  }, [count])
+  return(
+    <div>
+      <b>Counter Value is: {count}</b>  
+      <b> 👉🏽{EvenOdd}</b>
+    </div>
+  )
+}
 
+const Buttons = ()=>{
+  const count = useRecoilValue(countAtom);
+  const setCounter = useSetRecoilState(countAtom);
 
+  return(
+    <div>
+      <button onClick={()=>setCounter(coun=>coun+1)}>Increase</button>
+      <button onClick={()=>setCounter(coun=>coun-1)}>Decrease</button>
+    </div>
+  )
+}
--- recoil_deep_dive/src/atoms.jsx
+++ recoil_deep_dive/src/atoms.jsx
@@ -1,46 +1,6 @@
-import {atom, selector} from 'recoil'
+import { atom } from "recoil";
 
-export const networkAtom = atom({
-    key: 'networkAtom',
-    default: 100
+export const countAtom = atom({
+    key: 'countAtom',
+    default: 0
 })
-
-export const jobsAtom = atom({
-    key: 'jobsAtom',
-    default: 102
-})
-
-export const messagingAtom = atom({
-    key: 'messagingAtom',
-    default: 103
-})
-
-export const notificationsAtom = atom({
-    key: 'notificationsAtom',
-    default: 104
-})
-
-export const totalServiceSelector = selector({
-    key: 'totalServiceSelector',
-    get:({get})=>{
-        const networkValue = get(networkAtom);
-        const jobsValue = get(jobsAtom);
-        const messagingValue = get(messagingAtom);
-        const notificationsValue = get(notificationsAtom);
-        const total = networkValue + jobsValue + messagingValue + notificationsValue;
-        return total;
-
-    }
-})
-
-export const dataAtom = atom({
-    key: 'dataAtom',
-    default: {
-        network: 1,
-        jobs: 2,
-        messaging: 3,
-        notifications: 4
-    }
-})
-
-
